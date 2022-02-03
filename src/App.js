a5d3174d07932bc2110336972f2503c8aa0a49d1
Derriel <derrielcollins96@gmail.com>
2022-02-03 09:17:07 -0600

    Installed clarifai to use its api for the face detection, this will be moved to the backend later on

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { useState } from 'react';
 import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
 import './App.css';
 
-function App() {
-  const [input, setInput] = useState(0);
+console.log(Clarifai);
 
-  const onInputChange = (event) => {
-    console.log(event.target.value);
-  };
+const app = new Clarifai.App({
+ apiKey: 'fc631598ba864d589be77be7e1229c61'
+});
 
-  const onButtonSubmit = () => {
-    console.log('click');
-  };
-
-  const particleParams = {
+const particleParams = {
     "particles": {
       "color": {
         "value": "#ffffff"
@@ -79,6 +75,33 @@ function App() {
     }
     },
   };
+
+function App() {
+
+
+  const [input, setInput] = useState(0);
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+
+const onButtonSubmit = () => {
+ console.log("click");
+ app.models
+.predict(
+Clarifai.FACE_DETECT_MODEL,
+// THE JPG
+"https://i.insider.com/5d321d4ea209d3146d650b4a?width=1100&format=jpeg&auto=webp"
+)
+.then((response) => {
+ console.log(response);
+})
+.catch((err) => {
+ console.log(err);
+});
+};  
+  
 
   return (
     <div className="App">
